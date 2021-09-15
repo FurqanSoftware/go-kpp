f5ebe4a3473163dd1483b8f0439b688c4186bebc
Mahmud Ridwan <m@hjr265.me>
2021-09-15 18:08:27 +0600

    Print tests elaborately

--- package_test.go
+++ package_test.go
@@ -11,15 +11,38 @@ func TestPackage(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%v\n", pkg.Metadata())
+	fmt.Printf("Metadata: %v\n", pkg.Metadata())
 	stmts, err := pkg.Statements()
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%v\n", stmts)
+	fmt.Printf("Statements: %v\n", stmts)
 	tests, err := pkg.TestData()
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Printf("%v\n", tests)
+	fmt.Printf("Tests:\n")
+	for _, e := range tests {
+		fmt.Printf("  %v\n", e)
+		fmt.Printf("  Name: %s\n", e.Name())
+		fmt.Printf("  Group: %s\n", e.Group())
+		fmt.Printf("  Hint: %s\n", mustString(t)(e.Hint()))
+		fmt.Printf("  Description: %s\n", mustString(t)(e.Description()))
+		fmt.Println()
+	}
+	subms, err := pkg.Submissions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("Submissions: %v\n", subms)
+}
+
+func mustString(t *testing.T) func(s string, err error) string {
+	return func(s string, err error) string {
+		if err != nil {
+			t.Fatal(err)
+			return ""
+		}
+		return s
+	}
 }
